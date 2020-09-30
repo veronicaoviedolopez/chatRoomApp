cce1316798d5e42ed01ef4c244180daa57df83b4
veronica <veronicaoviedolopez@gmail.com>
2020-09-30 14:47:41 -0500

    constants updated

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,8 +1,11 @@
 export const constants = {
-  api: 'http://localhost:3001/api/',
+  // api: 'http://localhost:3001/api/',
+  https:'http://cryptic-harbor-61839.herokuapp.com/api/',
   JWTSecret: 'asdfghjkl',
   numeroSalt: 10,
-  IP_Server: 'http://localhost:3001',
-  client: 'http://localhost:3000/',
+  //IP_Server: 'http://localhost:3001',
+  IP_Server: 'http://cryptic-harbor-61839.herokuapp.com/',
+  //client: 'http://localhost:3000/',
+  client: 'http://cryptic-harbor-61839.herokuapp.com/',
   newMessageSound: "/sound/piece-of-cake.mp3"
 };
